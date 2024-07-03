8059360bba40662952594e83713e1714dbe0f4b4
Ale-p650 <ale.pavlinusich@gmail.com>
2024-07-03 12:18:47 -0300

    Commit Inicial

---
+++ Controllers/PascalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Logica;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PascalController : Controller
+    {
+        [HttpGet]
+        public string Get(int num)
+        {
+            return PascalCSV.Generate(num);
+        }
+    }
+}
---
+++ Logica/PascalCSV.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace WebApplication2.Logica
+{
+    public class PascalCSV
+    {
+        public static string Generate(int numRows)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int[][] pascal = new int[numRows][];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                pascal[i] = generaArr(i);
+            }
+
+
+
+            int[] generaArr(int num)
+            {
+                if (num == 0) return new int[] { 1 };
+                if (num == 1) return new int[] { 1, 1 };
+                else
+                {
+
+                    int[] arr = new int[num + 1];
+                    arr[0] = 1;
+                    arr[arr.Length - 1] = 1;
+
+
+                    for (int j = 1; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = pascal[num - 1][j - 1] + pascal[num - 1][j];
+
+                    }
+                    return arr;
+                }
+            }
+
+            for (int i = 0; i < pascal.Length; i++)
+            {
+                for (int j = 0; j < pascal[i].Length; j++)
+                {
+                    sb.Append(pascal[i][j]);
+                    sb.Append(" ");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
